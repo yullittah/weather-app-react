44600fa14d36ae0b74e3adb2d859d03e998403be
yullittah <yullittahmakuwe@gmail.com>
2024-10-14 08:37:26 +0200

    added axios

---
+++ src/application.js
@@ -0,0 +1,13 @@
+import React from "react";
+import bootstrap from "bootstrap";
+import axios from "axios";
+
+export default function WeatherApp() {
+  function showTemperature(response) {
+    alert(`the temperature in Harare is ${response.data.main.temp}°C`);
+  }
+  let apiKey = "f8df69650e7862f41c9077b3babedf21";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=Harare&appid=${apiKey}&units=metric`;
+
+  axios.get(url).then(showTemperature);
+}
